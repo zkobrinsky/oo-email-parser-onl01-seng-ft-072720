a7fe4baceeea8d2479470fda407ab8e4a85b3a26
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-08 13:24:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -16,6 +16,7 @@ def parse
   formatted_string = @email_addresses.gsub(", ", "*")
   formatted_string.gsub!(" ", "*")
   @email_addresses = string_to_array = formatted_string.split("*")
+  binding.pry
   @email_addresses.uniq!
 
   # binding.pry
